69c2624a8d753acf54ce82eb358f77b63bf808d7
Pavel A. Khanin <pkhanyn@gmail.com>
2021-10-29 17:51:43 +0300

    lesson 11.1 HW

--- spring-cloud-gateway/wait-for-service.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Starting to wait for Service at host '${1:-localhost}'"
-while ! nc -z -v -w30 ${1:-localhost} ${2:-3306};
-do
-  echo "Waiting for '${1:-localhost}' to be ready";
-  sleep 5;
-done;
